ad2f1cfd3c39850766c0702a1b984b9d2e4b1543
dnjsxoghd <feelchok1234@gmail.com>
2025-10-24 19:13:32 +0900

    update

--- backend/study-blog-back/src/blog/config/cloudinary.config.ts
+++ backend/study-blog-back/src/blog/config/cloudinary.config.ts
@@ -1,9 +1,7 @@
 import {v2 as cloudinary} from 'cloudinary';
 
 cloudinary.config({
-    cloud_name : 'doxscmwgl',
-    api_key : '199731363346568',
-    api_secret : 'bthQN2IdZBhHjgIDJfVun7gG0A8'
+
 })
 
 export default cloudinary;
