76c38770f486c66475d7d62555412202310a51b4
dnjsxoghd <feelchok1234@gmail.com>
2025-11-08 16:58:37 +0900

    update

--- Study-Blog/src/pages/Test2.tsx
+++ Study-Blog/src/pages/Test2.tsx
@@ -1,17 +1,8 @@
-import { useQuery } from "@tanstack/react-query";
 
 function Test2() {
 
-  const {data} = useQuery<{userId : string, id : number, title : string, body : string}, Error>({
-    queryKey : ['kk'],
-    queryFn : async ()=>{
-      const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-      return response.json();
-    },
-    staleTime : 5000
-  })
 
-  console.log(data);
+
 
   return (
     <>
